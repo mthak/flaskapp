6b4bc97668e5cfe5dfbd9436ecfc61ec64e641e0
root <root@mub16.vpc.cloudera.com>
2016-12-12 00:20:57 -0800

    Adding first run of a 3 tier app using pythong flask The app reads and returns hashsun of a message if exists it uses nginx for web flask for app and mongodb for the database tier All 3 tiers are baked in to docker containers and linked via docker-compose

---
+++ myflask/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify, abort
+from pymongo import MongoClient
+import hashlib
+import logging
+app = Flask(__name__)
+
+
+@app.route('/messages', methods=['POST'])
+def main_handler():
+    '''
+       Main routing for requests
+    '''
+    if request.method == 'POST':
+        return parse_message(request.get_json())
+    else:
+        return message_retrieve(hash_val)
+
+
+def parse_message(msg):
+    '''POST method to parse messages message 
+        and return sha1sum for the message
+        and save it in the mongodb
+    '''
+    data = msg['message']
+    result = db.collection.find_one({'message': data})
+    print " result is ", result
+    if result is None:
+        shasum_msg = hashlib.sha256(data).hexdigest()
+        msgdict = {}
+        msgdict['message'] = data
+        msgdict['digest'] = shasum_msg
+        logging.info(" print message stored in db is %s ", msgdict)
+        db.collection.insert_one(msgdict)
+        return msgdict['digest']
+    else:
+        exception = dict(
+            {"message": "A message with this value already exist"})
+        raise Exception(exception)
+
+
+@app.route('/messages/<hash_val>', methods=['GET'])
+def message_retrieve(hash_val):
+    ''' GET method check if a message with provided sha1sum 
+        exists in the data store
+        is yes return the msssage
+    '''
+    print hash_val
+    result = db.collection.find_one(
+        {"digest": hash_val}, {"message": 1, "_id": 0})
+    print "result is ", result
+    if result is not None:
+        return jsonify(**result)
+    else:
+        return abort(404,'{"Err_msg":"Message not found"}')
+if __name__ == "__main__":
+    LOG_FORMAT = '[%(asctime)s] %(process)d %(module)-12s %(levelname)-8s %(message)s'
+    DATE_FORMAT = '%d/%b/%Y %H:%M:%S %z'
+    logging.basicConfig(level=logging.INFO,
+                        format=LOG_FORMAT, datefmt=DATE_FORMAT)
+    client = MongoClient('mongodb://mongohost/27017/')
+    db = client['messages']
+    collection = db['message-info']
+    app.run(debug=True, host='0.0.0.0')
